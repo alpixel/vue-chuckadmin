33eca181e374e50b382ca6030c4e1f9647216125
Mickaël SURREL <mickael@alpixel.fr>
2017-03-07 18:08:01 +0100

    Debug webpack

--- webpack.config.js
+++ webpack.config.js
@@ -38,18 +38,17 @@ module.exports = {
         use: [
           'style-loader',
           'css-loader?importLoaders=1',
-          // ,
-          // {
-          //   loader: 'postcss-loader',
-          //   options: {
-          //     plugins: function () {
-          //       return [
-          //         require('precss'),
-          //         require('autoprefixer')
-          //       ];
-          //     }
-          //   }
-          // },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('precss'),
+                  require('autoprefixer')({ browsers: ['last 2 versions', '> 1%', 'Safari >= 8']})
+                ];
+              }
+            }
+          },
           'less-loader'
         ]
       }
