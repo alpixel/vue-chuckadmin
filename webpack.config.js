752dd3f7ec9e9e37c5c4ee74d413932d8b2daa28
Mickael SURREL <mickael@alpixel.fr>
2017-04-22 13:59:31 +0200

    Test pink bgcolor

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,32 @@ var path = require('path')
 var webpack = require('webpack')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
+// Basis to 'dev' and 'prod' environment
 module.exports = {
-  entry: [
-    './src/js/main.js'
-  ],
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
-  },
-  module: {
+    entry: [
+      './src/js/main.js'
+    ],
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      publicPath: '/dist/',
+      filename: 'build.js'
+    },
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.common.js',
+        'tippy$': 'tippy.js/dist/tippy.js'
+      }
+    },
+    performance: {
+      hints: false
+    }
+}
+
+
+// Specific to 'prod' environment
+if (process.env.NODE_ENV === 'production') {
+
+  module.exports.module = {
     rules: [
       {
         test: /\.css$/,
@@ -54,36 +70,12 @@ module.exports = {
       { test: /\.[ot]tf$/, use: 'url-loader?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
       { test: /\.eot$/, use: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]' }
     ]
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.common.js',
-      'tippy$': 'tippy.js/dist/tippy.js'
-    }
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000
-    },
-    host: "192.168.33.10" // <--- If you work with vagrant
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map',
-  plugins: [
-    new ExtractTextPlugin('style.css')
-  ]
-}
-
-if (process.env.NODE_ENV === 'production') {
+  }
 
   module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
+
+  module.exports.plugins = [
+    new ExtractTextPlugin('style.css'),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
@@ -98,5 +90,63 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
-  ])
+  ]
+} 
+
+// Specific to 'dev' environment
+else {
+  module.exports.module = {
+    rules: [
+        {
+          test: /\.css$/,
+          use: [ 'style-loader', 'css-loader' ]
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            loaders: {
+            }
+          }
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]?[hash]'
+          }
+        },
+        {
+          test: /\.less$/,
+          use: [
+            'style-loader',
+            'css-loader?importLoaders=1',
+            'postcss-loader',
+            'less-loader'
+          ]
+        },
+        { test: /\.svg$/, use: 'url-loader?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]' },
+        { test: /\.woff$/, use: 'url-loader?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]' },
+        { test: /\.woff2$/, use: 'url-loader?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]' },
+        { test: /\.[ot]tf$/, use: 'url-loader?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
+        { test: /\.eot$/, use: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]' }
+      ]
+  }
+
+  module.exports.devServer = {
+    historyApiFallback: true,
+    noInfo: true,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    },
+    host: "192.168.33.10" // <--- If you work with vagrant
+  }
+
+  module.exports.devtool = '#eval-source-map'
 }
